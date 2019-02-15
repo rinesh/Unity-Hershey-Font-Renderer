2a32d38df62b58909e843e5ed2d173a15cf4966f
Rinesh Thomas <rinesh.t@gmail.com>
2019-02-15 12:04:20 +0530

    Hershey Vector Font Renderer for Unity

---
+++ Assets/HersheyFont/Samples/PositionPen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionPen : MonoBehaviour 
+{
+
+    public HersheyFontRenderer fontHeading;
+    public HersheyFontRenderer fontDrawingText;
+    public Transform Camera;
+    public Transform feather;
+	int idx = 0;
+	int total = 1;
+	string[] fonts = { "ASTROLOGY", "CURSIVE", "CYRILC 1", "CYRILLIC", "FUTURAL", "FUTURAM", "GOTHGBT", "GOTHGRT", "GOTHICENG", "GOTHICGER", "GOTHICITA", "GOTHITT", "GREEK", "GREEKC", "GREEKS", "MARKERS", "MATHLOW", "MATHUPP", "METEOROLOGY", "MUSIC", "ROWMAND", "ROWMANS", "ROWMANT", "SCRIPTC", "SCRIPTS", "SYMBOLIC", "TIMESG", "TIMESI", "TIMESIB", "TIMESR", "TIMESRB" };
+
+	// Use this for initialization
+	void Start () 
+    {
+		total = fonts.Length;
+		fontHeading.PrintString("Hershey Font " + fonts[idx], HersheyFontRenderer.HERSHEY.astrology);
+		fontDrawingText.DrawString("Unity3D                                                     ",HersheyFontRenderer.HERSHEY.astrology, Finish);
+	}
+
+    void Finish()
+    {
+		idx++;
+		if(idx>=total)
+			idx=0;
+		fontHeading.PrintString("Hershey Font " + fonts[idx]);
+		fontDrawingText.DrawString("Unity3D                                                     ", (HersheyFontRenderer.HERSHEY)idx, Finish);
+    }
+
+	// Update is called once per frame
+	void Update ()
+	{
+		feather.position = fontDrawingText.GetCursorPosition();
+	}
+}
